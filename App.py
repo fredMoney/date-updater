caa3f9bc2e193e87becd20dd9aab66fcb7684316
freddyNgoulou <ngouloujacques19@stud.ase.ro>
2025-11-08 16:00:01 +0200

    UPDATE: Made the process more streamlined KNOWN BUG: Folder navigation doesn't work so don't use it. Start the program where you want to update dates.

--- App.py
+++ App.py
@@ -1,11 +1,16 @@
 import os, datetime
 from time import sleep
+from pathlib import Path
+
+
+DELAY_SHORT = 0.1
+DELAY_LONG = 0.4
 
 
 def parseInput(user_in) -> list:
     if user_in == "exit":
         print("Program will now exit.")
-        sleep(1)
+        sleep(DELAY_LONG)
         exit()
 
     episodes = list()
@@ -23,7 +28,7 @@ def parseInput(user_in) -> list:
 
 def modifyDatetime(paths):
     for path in paths:
-        sleep(0.5)
+        sleep(DELAY_SHORT)
         os.utime(path, (datetime.datetime.now().timestamp(), datetime.datetime.now().timestamp()))
         print("Episode " + path.split("\\")[-1] + " updated.", end="\n")
     
@@ -33,10 +38,10 @@ def listEpisodes(path):
     ep_list = sorted(os.scandir(path), key=lambda ep: ep.name)
     i = 1
     for ep in ep_list:
-        sleep(0.1)
+        sleep(DELAY_SHORT)
         print(f"{i:03}", ep.name, sep=". ")
         i = i + 1
-    sleep(0.2)
+    sleep(DELAY_LONG)
     print("Pick episode(s): ")
     user_in = input()
     episodes = parseInput(user_in)
@@ -48,28 +53,81 @@ def changeDir(path):
     dir_list = [entry for entry in os.scandir(path) if entry.is_dir()]
     i = 1
     for dir in dir_list:
-        sleep(0.1)
+        sleep(DELAY_SHORT)
         print(f"{i:03}", dir.name, sep=". ")
         i = i + 1
-    sleep(0.2)
+    sleep(DELAY_LONG)
     print("Choose dir: ")
     user_in = input()
     path = dir_list[int(user_in) - 1].path
     os.chdir(path)
-    return path
+    path = os.getcwd()
+    print(path)
+
+
+def listHelp():
+    print(
+        "Usage:",
+        "Navigate to the folder the series is in on disk. List the episodes. Pick an episode(s) to update the date.",
+        "To specify multiple episodes, delimitate them by commas (eg. 2, 3, 4); to specify a range, use hyphen",
+        "(eg. 2-6). Type \"exit\" to exit.",
+        sep="\n"
+    )
+
+
+def listDirs(path):
+    with os.scandir(path) as dir:
+        for file in dir:
+            if not file.is_file():
+                sleep(DELAY_SHORT)
+                print(file.name, end="\n")
+
+
+def listDirOptions(path):
+    print(
+        "You are currently in " + path + ". Paste the absolute path to dir or choose one of the following:",
+        "1. List dirs in current path",
+        "2. Move to dir in current path",
+        "3. Move up dir",
+        "4. Go back",
+        sep="\n"
+    )
+
+
+def navigateDirs(path):
+    while True:
+        sleep(DELAY_LONG)
+        listDirOptions(path)
+        option = input()
+
+        if option == "1":
+            listDirs(path)
+        elif option == "2":
+            changeDir(path)
+        elif option == "3":
+            os.chdir("..")
+            path = os.getcwd()
+        elif option == "4":
+            break
+        else:
+            pathTo = Path(option)
+            if pathTo.is_absolute():
+                os.chdir(option)
+                path = os.getcwd()
+            else:
+                print("Path is invalid! Try again...")
 
 
 def listOptions(path):
     print(
         "You are currently in " + path + ". Choose one of the following:",
-        "1. List dir",
-        "2. Change dir",
-        "3. Move up dir",
-        "4. List episodes",
-        "5. Exit",
+        "1. Process episode(s)",
+        "2. Navigate to dir",
+        "3. List help",
+        "4. Exit",
         sep="\n"
     )
-    
+
 
 def main():
     path = os.getcwd()
@@ -79,24 +137,17 @@ def main():
         option = input()
 
         if option == "1":
-            with os.scandir(path) as dir:
-                for file in dir:
-                    if not file.is_file():
-                        sleep(0.2)
-                        print(file.name, end="\n")
-        elif option == "2":
-            path = changeDir(path)
-        elif option == "3":
-            os.chdir("..")
-            path = os.getcwd()
-        elif option == "4":
             listEpisodes(path)
-        elif option == "5":
-            print("Program will now exit.")
+        elif option == "2":
+            navigateDirs(path)
+        elif option == "3":
+            listHelp()
+        elif option == "4":
+            print("Goodbye!")
             sleep(1)
             exit()
         else:
-            print("Invalid option. Please consult the list.")
-        sleep(1)
+            print("Invalid option. Please consult option 3.")
+        sleep(DELAY_LONG)
 
 main()
